397292d848018e83864d2fd36bc5deedb28cb2e2
Zotyamester <szatmary.zoltan1222@gmail.com>
2020-11-25 10:17:51 +0100

    Remove debugging logs

--- static/script.js
+++ static/script.js
@@ -1,10 +1,6 @@
 function tick(item) {
-    console.log(item.style.backgroundColor);
-    if (item.style.backgroundColor == 'rgb(69, 137, 67)' || item.style.backgroundColor == '') {
-        console.log('asd');
+    if (item.style.backgroundColor == 'rgb(69, 137, 67)' || item.style.backgroundColor == '')
         item.style.backgroundColor = 'rgb(244, 67, 54)';
-    } else {
-        console.log('lol');
+    else
         item.style.backgroundColor = 'rgb(69, 137, 67)';
-    }
 }
